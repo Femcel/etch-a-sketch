789484cbaa4bc85c7cd75655b28ccc84c10562aa
Femcel <hugogfernandes0012@gmail.com>
2023-02-01 15:23:46 -0300

    Add class and permanently change div color on hover

--- javascript.js
+++ javascript.js
@@ -2,5 +2,9 @@ const containerDiv = document.getElementById("container");
 for(let i = 0; i < 256; i++) {
   let pixel = document.createElement("div");
   pixel.className = "pixels";
+  pixel.addEventListener("mouseover", (event => {
+    pixel.classList.add("color");
+
+  }));
   containerDiv.appendChild(pixel);
 }
