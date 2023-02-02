8f40e38e39e6f582a8e8b2a75b224ffb47b912d5
Femcel <hugogfernandes0012@gmail.com>
2023-02-02 17:29:11 -0300

    Colors are now picked randomly

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@ const refreshButton = document.getElementById("refresh");
 refreshButton.addEventListener("click", refreshCanvas);
 
 let canvasSize = 16;
+let r = 0;
+let g = 0;
+let b = 0;
 
 createCanvas(canvasSize);
 
@@ -15,7 +18,15 @@ function createCanvas(canvasSize){
   for(let i = 0; i < canvasSize ** 2; i++) {
     let pixel = document.createElement("div");
     pixel.className = "pixels";
-    pixel.addEventListener("mouseover", (event => {pixel.classList.add("color");}));
+    pixel.addEventListener("mouseover", (event => {
+      pixel.classList.add("color");
+      r = Math.random() * 255;
+      g = Math.random() * 255;
+      b = Math.random() * 255;
+      pixel.setAttribute("style", `background-color: rgb(${r}, ${g}, ${b});`);
+    }));
+    
+    
     containerDiv.appendChild(pixel);
   }
 }
